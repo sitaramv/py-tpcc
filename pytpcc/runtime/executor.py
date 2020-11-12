a6140d056f024cbd4dffeb1636b824f1aa50058a
sharath chandra <sharujayaram@gmail.com>
2020-11-11 18:11:08 -0800

    re-enabling logging for failed transactions

--- pytpcc/runtime/executor.py
+++ pytpcc/runtime/executor.py
@@ -67,6 +67,7 @@ class Executor:
             except KeyboardInterrupt:
                 return -1
             except (Exception, AssertionError), ex:
+                logging.warning("Failed to execute Transaction '%s': %s" % (txn, ex))
                 if debug: traceback.print_exc(file=sys.stdout)
                 if self.stop_on_error: raise
                 r.abortTransaction(txn_id)
