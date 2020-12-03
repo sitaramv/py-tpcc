5c44557f173235c05205df8101307fa8add093f4
Kamini Jagtiani <kamini.jagtiani@couchbase.com>
2020-12-03 14:32:09 -0800

    To enable py-tpcc with collections

---
+++ pytpcc/util/run_sqlcollections.sh
@@ -0,0 +1,10 @@
+Url=${1:-127.0.0.1:8093}
+Site=http://$Url/query/service
+while read line;
+do
+sql1=$line
+qc="&query_context=default:default.tpcc"
+sql="${sql1} ${qc}"
+echo curl -u Administrator:password -v $Site  -d statement="$sql" 
+curl -u Administrator:password -v $Site  -d statement="$sql" 
+done
