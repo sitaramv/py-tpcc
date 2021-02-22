44696e77fc0b3a932add8b9e1e018c2d067cc0d7
sitaramv <sitaram.vemulapalli@couchbase.com>
2021-02-22 11:29:38 -0800

    move begin outside for

--- pytpcc/drivers/nestcollectionsdriver.py
+++ pytpcc/drivers/nestcollectionsdriver.py
@@ -586,10 +586,10 @@ class NestcollectionsDriver(AbstractDriver):
         o_carrier_id = params["o_carrier_id"]
         ol_delivery_d = params["ol_delivery_d"]
 
+        rs, status = runNQuery("begin", self.prepared_dict[ txn + "beginWork"],"",self.delivery_txtimeout, randomhost)
+        txid = rs[0]['txid']
         result = [ ]
         for d_id in range(1, constants.DISTRICTS_PER_WAREHOUSE+1):
-            rs, status = runNQuery("begin", self.prepared_dict[ txn + "beginWork"],"",self.delivery_txtimeout, randomhost)
-            txid = rs[0]['txid']
             newOrder, status = runNQueryParam(self.prepared_dict[ txn + "getNewOrder"], [d_id, w_id], txid, randomhost)
             if len(newOrder) == 0:
                 trs, self.tx_status = runNQuery("rollback", self.prepared_dict[ txn + "rollbackWork"],txid,"",randomhost)
