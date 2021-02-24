3c77c64bcd4303cca0d53d62253df5ba119e2b22
sitaramv <sitaram.vemulapalli@couchbase.com>
2021-02-23 22:10:17 -0800

    add new index

--- pytpcc/util/cbcrindexcollection_replicas.sql
+++ pytpcc/util/cbcrindexcollection_replicas.sql
@@ -1,7 +1,8 @@
 drop index CUSTOMER.CU_W_ID_D_ID_LAST USING GSI
 drop index DISTRICT.DI_ID_W_ID USING GSI
 drop index NEW_ORDER.D_ID_W_ID_O_ID USING GSI
-drop index ORDERS.OR_W_ID_D_ID_C_ID_O_ID USING GSI
+drop index ORDERS.OR_DID_WID_ID_CID USING GSI
+drop index ORDERS.OR_CID_DID_WID_ID_CAID_EID USING GSI
 drop index ORDER_LINE.OL_O_ID_D_ID_W_ID USING GSI
 drop index WAREHOUSE.WH_ID USING GSI
 drop primary index on CUSTOMER USING GSI
@@ -16,7 +17,8 @@ drop primary index on WAREHOUSE USING GSI
 CREATE INDEX CU_W_ID_D_ID_LAST on CUSTOMER(C_W_ID, C_D_ID, C_LAST) USING GSI WITH {"defer_build": true, "num_replica":replicas}
 CREATE INDEX DI_ID_W_ID on DISTRICT(D_ID, D_W_ID) USING GSI WITH {"defer_build": true, "num_replica":replicas}
 CREATE INDEX D_ID_W_ID_O_ID on NEW_ORDER(NO_D_ID, NO_W_ID, NO_O_ID) USING GSI WITH {"defer_build": true, "num_replica":replicas}
-CREATE INDEX OR_W_ID_D_ID_C_ID_O_ID on ORDERS(O_W_ID, O_D_ID, O_C_ID, O_ID DESC) USING GSI WITH {"defer_build": true, "num_replica":replicas}
+CREATE INDEX OR_DID_WID_ID_CID on ORDERS(O_D_ID, O_W_ID, O_ID, O_C_ID) USING GSI WITH {"defer_build": true, "num_replica":replicas}
+CREATE INDEX OR_CID_DID_WID_ID_CAID_EID on ORDERS(O_C_ID, O_D_ID, O_W_ID, O_ID DESC, O_CARRIER_ID, O_ENTRY_D) USING GSI WITH {"defer_build": true, "num_replica":replicas}
 CREATE INDEX OL_O_ID_D_ID_W_ID on ORDER_LINE(OL_O_ID, OL_D_ID, OL_W_ID) USING GSI WITH {"defer_build": true, "num_replica":replicas}
 CREATE INDEX WH_ID on WAREHOUSE(W_ID) USING GSI WITH {"defer_build": true, "num_replica":replicas}
 select keyspace_id, state from system:indexes
